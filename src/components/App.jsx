929418cabab05e6198f76ce515ca2529e35ec736
shashankg32 <shashank6511@gmail.com>
2023-03-06 19:55:00 +0530

    increment and decrement button using state and hooks video no 420

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
+import React,{useState} from "react";
+
+function App() {
+  
+  const [count, setCount] = useState(0);
+  
+  function increment(){
+    setCount(count + 1);
+  }
+
+  function decrement(){
+    setCount(count - 1);
+  }
+
+
+  return <div className="container">
+  <h1>{count}</h1>
+  <button onClick={increment}>+</button>
+  <button onClick={decrement}>-</button>
+  </div>;
+}
+
+export default App;
+
