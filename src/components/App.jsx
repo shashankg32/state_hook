3850d608fed4b3920e6e2d885dcbef99b8001baa
shashankg32 <shashank6511@gmail.com>
2023-03-06 20:12:20 +0530

    nothing changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
-import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
 import React,{useState} from "react";
-
 function App() {
-  
-  const [count, setCount] = useState(0);
-  
+
+  var [count, setCount] = useState(0);
+
   function increment(){
     setCount(count + 1);
   }
@@ -13,13 +11,11 @@ function App() {
     setCount(count - 1);
   }
 
-
-  return <div className="container">
-  <h1>{count}</h1>
-  <button onClick={increment}>+</button>
-  <button onClick={decrement}>-</button>
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
   </div>;
 }
 
 export default App;
-
