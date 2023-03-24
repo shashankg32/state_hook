04b30bfc54a984681e76139965da76a49884e076
shashankg32 <shashank6511@gmail.com>
2023-03-24 21:10:06 +0530

    dynamic time with react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+
 function App() {
+  setInterval(updateTime, 1000);
 
-  var [count, setCount] = useState(0);
+  const now = new Date().toLocaleTimeString();
 
-  function increment(){
-    setCount(count + 1);
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
 
-  function decrement(){
-    setCount(count - 1);
-  }
-
-  return <div>
-    <h1>{count}</h1>
-    <button onClick={increment}>+</button>
-    <button onClick={decrement}>-</button>
-  </div>;
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
 }
 
 export default App;
